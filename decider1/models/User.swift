a232ccfcbe217d6e06fedeba975dfc0590515965
Oto <oto_machavariani_ai@geolab.edu.ge>
2018-07-19 17:32:58 -0700

    Add SetCurrent(User) Functrion

--- decider1/models/User.swift
+++ decider1/models/User.swift
@@ -14,12 +14,12 @@ import FirebaseDatabase.FIRDataSnapshot
 
 class User: Codable {
     
-   
+    
     
     let uid: String
     let username: String
     
-   
+    
     
     init(uid: String, username: String) {
         
@@ -28,29 +28,34 @@ class User: Codable {
         self.username = username
     }
     init?(snapshot: DataSnapshot) {
-    guard let dict = snapshot.value as? [String : Any],
-    let username = dict["username"] as? String
-    else { return nil }
-    
-    self.uid = snapshot.key
-    self.username = username
+        guard let dict = snapshot.value as? [String : Any],
+            let username = dict["username"] as? String
+            else { return nil }
+        
+        self.uid = snapshot.key
+        self.username = username
     }
     
     private static var _current: User?
     
-   
-    static var current: User {
-    
-        guard let currentUser = _current else {
-            fatalError("Error: current user doesn't exist")
-        }
-        
-     
-        return currentUser
+    static func setCurrent(_ user: User) {
+        self._current = user
     }
+    
+    
+    //    static var current: User {
+    //
+    //        guard let currentUser = _current else {
+    //            fatalError("Error: current user doesn't exist")
+    //        }
+    //
+    //
+    //        return currentUser
+    //    }
     
     
     
 }
+
 
 
